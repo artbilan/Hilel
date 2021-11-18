ab689a0d70cbfeb9bbacbeb1a100bf3c311ef455
Art <artem.bilan13@gmail.com>
2021-11-18 12:11:23 +0200

    3d commit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
-link = "https://lms.ithillel.ua/"
+
+link = "https://lms.ithillel.ua"
 
 response = requests.get(link)
-print(response)
+if response.ok:
+    response = response.text
+    with open("link.html", "w", encoding="utf8") as file:
+        file.write(response)
+soup = BeautifulSoup(response, "lxml")
+print(soup)
