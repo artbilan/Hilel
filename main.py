2d631dbc28a74a6efadcab426bc05cec0743dd9f
Art <artem.bilan13@gmail.com>
2021-11-18 14:37:53 +0200

    update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 
 
 link = "https://lms.ithillel.ua"
-
 response = requests.get(link)
 if response.ok:
     response = response.text
     with open("link.html", "w", encoding="utf8") as file:
         file.write(response)
+
 soup = BeautifulSoup(response, "lxml")
 print(soup)
