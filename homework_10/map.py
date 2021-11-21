33903ce9ea3abec3b7be1806a43739bb03dd49bb
Art <artem.bilan13@gmail.com>
2021-11-21 10:30:37 +0200

    update

--- homework_10/map.py
+++ homework_10/map.py
@@ -1,2 +1,11 @@
+def new_map(update_string: str, other_list: list) -> list:
+    new_list = []
+    for i in other_list:
+        new_list.append(f'{update_string} {i}')
+    return new_list
+
+
 names = ["John", "Robert", "Marta"]
 
+new_li = new_map("Hello", names)
+print(new_li)
