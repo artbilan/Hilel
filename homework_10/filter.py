730baef6a38419c56c5ad96acf082fbf2e84bf82
Art <artem.bilan13@gmail.com>
2021-11-25 18:16:18 +0200

    update

--- homework_10/filter.py
+++ homework_10/filter.py
@@ -1,17 +1,26 @@
-def new_filter(number: int, number_list: list, smaller=False) -> list:
-    new_list = []
-    if smaller is False:
-        for i in number_list:
-            if i > number:
-                new_list.append(i)
-        return new_list
-    elif smaller is True:
-        for i in number_list:
-            if i < number:
-                new_list.append(i)
-        return new_list
+import re
+
+
+def new_filter(lam, new_list):
+    new_li = []
+    new_int = None
+    lam = str(lam)
+    cc = re.findall('\d+', lam)
+    for i in cc:
+        if i.isdigit():
+            new_int = int(i)
+    if "<" in lam:
+        for i in new_list:
+            if i < new_int:
+                new_li.append(i)
+    elif ">" in lam:
+        for i in new_list:
+            if i > new_int:
+                new_li.append(i)
+    return new_li
 
 
 li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 500, 499, 900, -10]
-search = new_filter(-1, li, smaller=True)  # smaller is False - search from right side, smaller is True - left side
-print(search)
+
+a = new_filter("< 500", li)
+print(a)
