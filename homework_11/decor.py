463934e7f42c246951e55b970e77d4fe7b47a359
Art <artem.bilan13@gmail.com>
2021-11-25 18:49:20 +0200

    update

--- homework_11/decor.py
+++ homework_11/decor.py
@@ -2,15 +2,16 @@ from typing import Callable
 
 
 def smart_divide(func: Callable) -> Callable:
+    print(f'start from function {smart_divide.__name__}')
+
     def inner(a: int, b: int) -> Callable:
-        print(f'start from function "{inner.__name__}"')
         return func(a, b)
     return inner
 
 
 @smart_divide
-def divide(a: int, b: int) -> str:
-    print(f'result decoration func is {a/b}')
+def div(a: int, b: int) -> str:
+    print(f'result is {a/b}')
 
+div(9, 13)
 
-divide(9, 5)
