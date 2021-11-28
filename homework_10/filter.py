6bdf05f1f8bfd8b12b0cc446d6dbb8aec8053f99
Art <artem.bilan13@gmail.com>
2021-11-28 19:10:03 +0200

    update

--- homework_10/filter.py
+++ homework_10/filter.py
@@ -1,26 +1,15 @@
-import re
+from typing import Callable
 
 
-def new_filter(lam, new_list):
-    new_li = []
-    new_int = None
-    lam = str(lam)
-    cc = re.findall('\d+', lam)
-    for i in cc:
-        if i.isdigit():
-            new_int = int(i)
-    if "<" in lam:
-        for i in new_list:
-            if i < new_int:
-                new_li.append(i)
-    elif ">" in lam:
-        for i in new_list:
-            if i > new_int:
-                new_li.append(i)
-    return new_li
+def new_filter(func: Callable, li: list) -> list:
+    new_list = []
+    for i in li:
+        if func(i):
+            new_list.append(i)
+    return new_list
 
 
-li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 500, 499, 900, -10]
+a = (-7, 6, 0, 2, 100, 49)
 
-a = new_filter("> 500", li)
-print(a)
+cc = new_filter(lambda x: x > 5, a)
+print(cc)
