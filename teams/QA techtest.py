42a056aab4b156480ab2cb476df850839f4807e1
Art <artem.bilan13@gmail.com>
2021-12-09 11:47:42 +0200

    update

--- teams/QA techtest.py
+++ teams/QA techtest.py
@@ -14,39 +14,43 @@ ab = math.sqrt((ax - bx) ** 2 + (ay - by) ** 2)
 ac = math.sqrt((ax - cx) ** 2 + (ay - cy) ** 2)
 bc = math.sqrt((bx - cx) ** 2 + (by - cy) ** 2)
 
-print(f'Length of AB is : {ab}')
-print(f'Length of BC is : {bc}')
-print(f'Length of AC is : {ac}')
+if ab + ac <= bc or ac + bc <= ab or ab + bc <= ac:
+    print("This is not Triangle")
+else:
+
+    print(f'Length of AB is : {ab}')
+    print(f'Length of BC is : {bc}')
+    print(f'Length of AC is : {ac}')
 
 
 # является ли треугольник равносторонним
-if math.sqrt(bc) - math.sqrt((ab + ac)) == 0:
-    print("Triangle is Equilateral")
-else:
-    print("Triangle is not Equilateral")
+    if math.sqrt(bc) - math.sqrt((ab + ac)) <= 0:
+        print("Triangle is Equilateral")
+    else:
+        print("Triangle is not Equilateral")
 
 # является ли треугольник равнобедренным
-if ab == bc or ab == ac or bc == ab or bc == ac:
-    print("Triangle is Isosceles")
-else:
-    print("Triangle is not Isosceles")
+    if ab == bc or ab == ac or bc == ab or bc == ac:
+        print("Triangle is Isosceles")
+    else:
+        print("Triangle is not Isosceles")
 
 # является ли треугольник прямоугольным
-a = (ax - ay)**2
-b = (bx - by)**2
-c = (cx - cy)**2
+    a = (ax - ay)**2
+    b = (bx - by)**2
+    c = (cx - cy)**2
 
-if math.sqrt(c) == math.sqrt(a) + math.sqrt(b):
-    print("Triangle is Right")
-else:
-    print("NOT Right Triangle")
+    if math.sqrt(c) == math.sqrt(a) + math.sqrt(b):
+        print("Triangle is Right")
+    else:
+        print("NOT Right Triangle")
 
 # периметр треугольника
-perimeter = ab + ac + bc
-print(f'Perimeter is: {perimeter}')
+    perimeter = ab + ac + bc
+    print(f'Perimeter is: {perimeter}')
 
 # все чётные числа от 0 до величины периметра треугольника включительно
-print("Parity number in range from 0 to triangle perimeter: ")
-for i in range(math.floor(perimeter)):
-    if i % 2 == 0:
-        print(i)
+    print("Parity number in range from 0 to triangle perimeter: ")
+    for i in range(math.floor(perimeter)):
+        if i % 2 == 0:
+            print(i)
